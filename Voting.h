51a33b1ad8f53adb85d9bab63ae175d0271d3251
HowardDennis <h2dennis@gmail.com>
2015-09-20 14:18:11 -0500

    Update Voting.h

--- Voting.h
+++ Voting.h
@@ -25,8 +25,6 @@ void voting_read_names (istream& r, int numNames, vector<string>& names);
 // voting_eval
 // ------------
 
-void get_ballots (istream& r, vector<string>& ballots);
-
 void get_ballots2 (istream& r, vector< vector<int> >& ballots);
 
 // -------------
