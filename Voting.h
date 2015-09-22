cbf3222ff3707ab853d22c632db457068251ef3e
HowardDennis <h2dennis@gmail.com>
2015-09-22 15:18:31 -0500

    Update Voting.h

--- Voting.h
+++ Voting.h
@@ -30,6 +30,8 @@ struct Candidate
     Candidate():name(""), eliminated(false){}
 };
 
+void determine_winner (vector<Candidate>& cans);
+
 void get_candidates (istream& r, int numNames, vector<Candidate>& cans);
 
 void voting_read_names (istream& r, int numNames, vector<string>& names);
