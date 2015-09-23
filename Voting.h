6a68e2455dc520c8f2b4fbaba8c1e6cbdf24a53b
HowardDennis <h2dennis@gmail.com>
2015-09-22 19:14:29 -0500

    Update Voting.h

--- Voting.h
+++ Voting.h
@@ -34,19 +34,11 @@ bool winner (vector<Candidate>& cans);
 
 void get_candidates (istream& r, int numNames, vector<Candidate>& cans);
 
-void voting_read_names (istream& r, int numNames, vector<string>& names);
-
-void get_ballots (istream& r, vector< vector<int> >& ballots);
-
-void get_ballots2 (istream& r, vector<Candidate>& candidates);
+void get_ballots (istream& r, vector<Candidate>& candidates);
 
 void assign_ballot (vector<Candidate>& candidates, int column, vector<int> ballots);
 
-void count_votes (vector< vector<int> >& ballots, vector< vector<int> >& votes, int column);
-
 void voting_solve (istream& r, ostream& w);
-
-void print_all (vector<Candidate>& v);
 
 #endif // Voting_h
 
