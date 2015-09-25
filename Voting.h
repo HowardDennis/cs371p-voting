433987ab99c5f4fc60bddbd38a13bcb6c27ea875
HowardDennis <h2dennis@gmail.com>
2015-09-24 19:34:06 -0500

    Update Voting.h

--- Voting.h
+++ Voting.h
@@ -11,47 +11,22 @@
 // includes
 // --------
 
-#include <cassert>  // assert
-#include <iostream> // endl, istream, ostream
-#include <sstream>  // istringstream
-#include <string>   // getline, string
+#include <cassert>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
 extern int numVotes;
 
-struct Candidate
-{
-    string name;
-    vector< vector<int> > votes;
-    bool elim;
-
-    Candidate():name(""), elim(false){}
-};
-
-bool reassign2 (vector<Candidate>& candidates, vector<Candidate>& losers);
-
-void eliminate_zero (vector<Candidate>& can, vector<Candidate>& losers);
-
-bool reassign (vector<Candidate>& candidates, int column, vector<Candidate>& losers);
-
-void case_eval (istream& r, ostream& w);
-
-bool eliminate (vector<Candidate>& can, vector<Candidate>& losers);
-
-bool is_tie (vector<Candidate>& cans, ostream& w, vector<Candidate>& losers);
-
-bool winner (vector<Candidate>& cans, ostream& w, vector<Candidate>& losers);
-
-void get_candidates (istream& r, int numNames, vector<Candidate>& cans);
-
-void get_ballots (istream& r, vector<Candidate>& candidates, int numNames);
-
-void assign_ballot (vector<Candidate>& candidates, int column, vector<int>& ballots);
-
-void voting_solve (istream& r, ostream& w);
-
+bool winner (vector<int>& count, vector<string>& electees, vector<int>& losers, vector<string>& candidates, vector<int>& losers_total);
+void print (ostream w, vector<string>& electees);
+void elect (istream& r, ostream& w);
+void run_cases (istream& r, ostream& w);
 #endif // Voting_h
 
 
