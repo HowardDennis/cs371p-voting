d5901778767c2a662a33b104d325a386294732b1
HowardDennis <h2dennis@gmail.com>
2015-09-24 20:27:25 -0500

    Update Voting.h

--- Voting.h
+++ Voting.h
@@ -21,11 +21,12 @@
 
 using namespace std;
 
-extern int numVotes;
-
-bool winner (vector<int>& count, vector<string>& electees, vector<int>& losers, vector<string>& candidates, vector<int>& losers_total);
-void print (ostream w, vector<string>& electees);
+void reassign(vector<int>& count, vector<int>& losers, vector<vector<string> >& ballots, vector<int>& losers_total);
+bool check(int& v, vector<int>& losers_total);
+bool winner(vector<int>& count, vector<string>& electees, vector<int>& losers, vector<string>& candidates, vector<int>& losers_total);
+void sortVotes(istream& r, vector< vector<string> >& ballots, vector<int>& count, string& v);
 void elect (istream& r, ostream& w);
+void print(ostream& w, vector<string>&  winners)
 void run_cases (istream& r, ostream& w);
 #endif // Voting_h
 
